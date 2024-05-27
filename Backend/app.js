5ecdd0cfd5020fa10e9e60c1976df075db3e9c20
kraken <solodarken@gmail.com>
2024-05-27 17:52:59 +0530

    some changes

--- Backend/app.js
+++ Backend/app.js
@@ -4,32 +4,35 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 import userRouter from "./routes/userRouter.js";
-import applicationRouter  from "./routes/applicationRouter.js";
+import applicationRouter from "./routes/applicationRouter.js";
 import jobRouter from "./routes/jobRouter.js";
-import {dbConnection} from "./database/dbConnection.js";
-import {errorMiddleware}from "./middlewares/error.js"
+import { dbConnection } from "./database/dbConnection.js";
+import { errorMiddleware } from "./middlewares/error.js";
 
+const app = express();
+dotenv.config({ path: "./config/config.env" });
 
-
-const app=express();
-dotenv.config({path: "./config/config.env"});
-
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://jobzeegunand-1.onrender.com",
+  }),
+);
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(fileUpload({
-    useTempFiles:true,
-    tempFileDir:"/tmp/",
-})
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  }),
 );
-app.use("/api/v1/user",userRouter);
-app.use("/api/v1/application",applicationRouter);
-app.use("/api/v1/job",jobRouter); 
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/application", applicationRouter);
+app.use("/api/v1/job", jobRouter);
 
 dbConnection();
 
-app.use(errorMiddleware); 
-export default app; 
+app.use(errorMiddleware);
+export default app;
